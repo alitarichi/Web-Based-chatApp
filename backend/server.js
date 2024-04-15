d1dd80bb2ab600c59246a8e93aea3ec04f8f8948
ali tarichi <tarichiali@gmail.com>
2024-04-15 15:31:30 +0300

    user,routes inital commit

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -11,9 +14,11 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser()); // to parse incoming cookies from req.cookies
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", messageRoutes, userRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();

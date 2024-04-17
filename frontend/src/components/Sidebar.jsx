15a3e3ae2ed3cdf9d16bebc4c6f39bb13e31ba75
ali tarichi <tarichiali@gmail.com>
2024-04-17 16:31:21 +0300

    logout button created

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,13 +1,14 @@
 import Conversations from "./Conversations";
+import LogoutButton from "./LogoutButton";
 import SearchInput from "./SearchInput";
 
 const sidebar = () => {
   return (
-    <div>
+    <div className="border-r border-slate-500 p-4 flex flex-col">
       <SearchInput />
       <div className="divider px-3"></div>
       <Conversations />
-      {/* <LogoutButton />  */}
+      <LogoutButton />
     </div>
   );
 };
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
   return (
     <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding packdrop-filter backdrop-blur-lg bg-opacity-0">
       <Sidebar />
+      <MessageContainer />
     </div>
   );
 };

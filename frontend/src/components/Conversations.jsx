3d6421ab30c70cbaf20776d5cede0bf17e33828c
ali tarichi <tarichiali@gmail.com>
2024-04-22 16:27:35 +0300

    useConversations inital commit

--- frontend/src/components/Conversations.jsx
+++ frontend/src/components/Conversations.jsx
@@ -1,6 +1,8 @@
+import useGetConversations from "../hooks/useGetConversations";
 import Conversation from "./conversation";
 
 const Conversations = () => {
+  useGetConversations();
   return (
     <div className="py-2 flex flex-col overflow-auto">
       <Conversation />

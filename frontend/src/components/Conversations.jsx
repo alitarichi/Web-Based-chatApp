a27c040cc43bfc4e990a857e5e79d97c91da4d60
ali tarichi <tarichiali@gmail.com>
2024-04-22 16:34:46 +0300

    emojis imported from githuub

--- frontend/src/components/Conversations.jsx
+++ frontend/src/components/Conversations.jsx
@@ -2,14 +2,20 @@ import useGetConversations from "../hooks/useGetConversations";
 import Conversation from "./conversation";
 
 const Conversations = () => {
-  useGetConversations();
+  const { loading, conversations } = useGetConversations();
+  console.log("CONVERSATIONS:", conversations);
   return (
     <div className="py-2 flex flex-col overflow-auto">
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
+      {conversations.map((conversation) => (
+        <Conversation
+          key={conversation._id}
+          conversation={conversation}
+          emoji
+        />
+      ))}
+      {loading ? (
+        <span className="LOADING loading-spinner mx-auto"></span>
+      ) : null}
     </div>
   );
 };

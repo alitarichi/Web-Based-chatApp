3efd82a2d447e070b47f7cf686d5ec7801f77942
ali tarichi <tarichiali@gmail.com>
2024-04-22 15:56:45 +0300

    login error handling is done. login & logout & signup works as should

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import UseLogin from "../../hooks/UseLogin";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { loading, login } = UseLogin();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(username, password);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
@@ -10,7 +21,7 @@ const Login = () => {
           <span className="text-blue-500"> Chat App </span>
         </h1>
 
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div>
             <label className="label p-2">
               <span className="text-base label-text">Username</span>
@@ -19,6 +30,8 @@ const Login = () => {
               type="text"
               placeholder="Enter Username"
               className="w-full input input-bordered h-10"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
             />
           </div>
 
@@ -30,12 +43,21 @@ const Login = () => {
               type="password"
               placeholder="Enter Username"
               className="w-full input input-bordered h-10"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
           <div>
-            <button className="btn btn-block btn-sm mt-2 border border-slate-700">
-              Login
+            <button
+              className="btn btn-block btn-sm mt-2 border border-slate-700"
+              disabled={loading}
+            >
+              {loading ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                "Login"
+              )}
             </button>
           </div>
 

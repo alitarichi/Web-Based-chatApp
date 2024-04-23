2cb6d3895b876d2760834ca5dd2353485e9ad1b4
ali tarichi <tarichiali@gmail.com>
2024-04-23 12:25:55 +0300

    send messages function works. custom hook created

--- frontend/src/hooks/useSendMessage.js
+++ frontend/src/hooks/useSendMessage.js
@@ -3,14 +3,14 @@ import useConversation from "../zustand/useConversation";
 import toast from "react-hot-toast";
 
 const useSendMessage = () => {
-  const [loading, setLoading] = useState;
+  const [loading, setLoading] = useState();
   const { messages, setMessages, selectedConversation } = useConversation();
 
   const sendMessage = async (message) => {
     setLoading(true);
     try {
       const res = await fetch(
-        `/api/messages/send/:id${selectedConversation._id}`,
+        `/api/messages/send/${selectedConversation._id}`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },

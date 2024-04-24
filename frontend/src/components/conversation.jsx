df594c737fb46614f680027605f35848ca7bd5b4
ali tarichi <tarichiali@gmail.com>
2024-04-24 14:22:44 +0300

    online green light implanted

--- frontend/src/components/conversation.jsx
+++ frontend/src/components/conversation.jsx
@@ -1,9 +1,12 @@
+import { useSocketContext } from "../Context/SocketContext";
 import useConversation from "../zustand/useConversation";
 
 const conversation = ({ conversation, lastIdx, emoji }) => {
   const { selectedConversation, setSelectedConversation } = useConversation();
 
   const isSelected = selectedConversation?._id === conversation._id;
+  const { onlineUsers } = useSocketContext();
+  const isOnline = onlineUsers.includes(conversation._id);
   return (
     <>
       <div
@@ -12,7 +15,7 @@ const conversation = ({ conversation, lastIdx, emoji }) => {
         } `}
         onClick={() => setSelectedConversation(conversation)}
       >
-        <div className="avatar online">
+        <div className={` avatar ${isOnline ? "online" : ""}`}>
           <div className="w-12 rounded-full">
             <img src={conversation.profilePic} alt="user avatar" />
           </div>

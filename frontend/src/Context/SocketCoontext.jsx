6b4fd82e28f76ce58df4be03c519ba3516bb490d
ali tarichi <tarichiali@gmail.com>
2024-04-24 14:15:30 +0300

    miss spelling fixed

--- frontend/src/Context/SocketCoontext.jsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-export const SocketContext = createContext();
-
-export const SocketContextProvider = ({ children }) => {
-  return <SocketContext.Provider value={{}}>{children}</SocketContext.Provider>;
-};
